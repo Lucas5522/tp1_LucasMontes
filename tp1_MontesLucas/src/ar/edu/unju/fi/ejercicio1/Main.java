99694bff3e12e6273c08c5a9bc962909fe6d0230
Lucas5522 <luceitormaser@gmail.com>
2024-04-09 13:40:37 -0300

    Commit del ejercicio 1 finalizado y creacion del ejercicio 2

--- tp1_MontesLucas/src/ar/edu/unju/fi/ejercicio1/Main.java
+++ tp1_MontesLucas/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -1,5 +1,17 @@
 package ar.edu.unju.fi.ejercicio1;
 
 public class Main {
-
+	
+	public static void main(String[] args) {
+	
+	byte num1 = 8;
+	byte num2 = 1;
+	byte num3 = 5;
+	byte num4 = 20;
+	byte num5 = 9;
+		
+	float promedio = (num1 + num2 + num3 + num4 + num5) / 5.0f;	
+		System.out.println("el promedio de los numeros es:"+promedio);
+		
+	}
 }
