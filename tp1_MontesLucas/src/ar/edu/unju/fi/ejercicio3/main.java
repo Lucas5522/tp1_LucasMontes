413ebb285577e2056a857646de81736b3cdad466
Lucas5522 <luceitormaser@gmail.com>
2024-04-09 15:27:20 -0300

    commit finalizacion del ejercicio 3 y creacion del class del ejercicio 4

--- tp1_MontesLucas/src/ar/edu/unju/fi/ejercicio3/main.java
+++ tp1_MontesLucas/src/ar/edu/unju/fi/ejercicio3/main.java
@@ -1,9 +1,21 @@
 package ar.edu.unju.fi.ejercicio3;
 
+import java.util.Scanner;
+
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int num;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("ingrese un numero:");
+		num = scanner.nextInt();
+		if (num % 2 == 0) {
+			System.out.println("el numero ingresado es par y su triplo es: " + num * 3);
+		}else {
+			System.out.println("el numero ingresado es impar y su doble es: " + num * 2);
+		
+		}
 
 	}
 
