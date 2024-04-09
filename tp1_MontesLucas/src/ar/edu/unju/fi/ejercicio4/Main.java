cfafe8d78a9d632a3a26f68ea17b73f9e340ac0c
Lucas5522 <luceitormaser@gmail.com>
2024-04-09 16:14:03 -0300

    Commit finalizacion ejercicio 4 y creacion de class para el ejercicio 5

--- tp1_MontesLucas/src/ar/edu/unju/fi/ejercicio4/Main.java
+++ tp1_MontesLucas/src/ar/edu/unju/fi/ejercicio4/Main.java
@@ -1,10 +1,27 @@
 package ar.edu.unju.fi.ejercicio4;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int num;
+		int fact;
+		Scanner scanner = new Scanner(System.in);
+		do {
+		System.out.println("ingrese un numero entre [0,10] para hacer su factorial:");
+		num = scanner.nextInt();
+		if (num >10) {
+			System.out.println("ingrese nuevamente un valor entre 1 y 10");
+		}
+		} while (num > 10  );
+		System.out.println("el numero ingresado es: " + num);
+		fact = num;
+		while (num != 1) {
+			num = num - 1;
+			fact = fact*num;
+		}
+		System.out.println("el factorial es= " + fact);
 	}
 
 }
