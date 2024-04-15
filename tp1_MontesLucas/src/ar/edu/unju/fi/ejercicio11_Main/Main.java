38042e7c774a1268fb07fd558ed64550734ff7f2
Lucas5522 <luceitormaser@gmail.com>
2024-04-15 20:15:31 -0300

    Commit continuacion del tp1 creacion del package y class finalizacion ejercicio 11

---
+++ tp1_MontesLucas/src/ar/edu/unju/fi/ejercicio11_Main/Main.java
@@ -0,0 +1,15 @@
+package ar.edu.unju.fi.ejercicio11_Main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int total = 0;
+		for (int i = 2, j = 40; j>=1 ; i++ , j--) {
+			total = j*i;
+			System.out.println(total + "");
+			
+		}
+
+	}
+
+}
