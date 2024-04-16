b509498c98868aa9700d2f416183d5c13e7a07ae
Lucas5522 <luceitormaser@gmail.com>
2024-04-16 13:46:07 -0300

    Finalizacion ejercicio 15 Creacion de package y class ejercicio 16

--- tp1_MontesLucas/src/ar/edu/unju/fi/ejercicio15_Main/Main.java
+++ tp1_MontesLucas/src/ar/edu/unju/fi/ejercicio15_Main/Main.java
@@ -1,10 +1,28 @@
 package ar.edu.unju.fi.ejercicio15_Main;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ingrese un valor entre(3,10)");
+		a = scanner.nextInt();
+		int[] quince = new int[a];
+		for(int i = 0 ; i < quince.length; i++) {
+			System.out.println("Ingrese valores");
+			int n = scanner.nextInt();
+			quince[i]=n;	
+		}
+		for(int w = 0 ; w < a ; w++) {
+			System.out.println( "valor: " + quince[w]);
 
+		}
+		System.out.println("**************************");
+		for(int f = a-1 ; f >= 0 ; f--) {
+			System.out.println( "valor: " + quince[f]);
+
+		}
 	}
-
 }
---
+++ tp1_MontesLucas/src/ar/edu/unju/fi/ejercicio16_Main/Main.java
@@ -0,0 +1,5 @@
+package ar.edu.unju.fi.ejercicio16_Main;
+
+public class Main {
+
+}
