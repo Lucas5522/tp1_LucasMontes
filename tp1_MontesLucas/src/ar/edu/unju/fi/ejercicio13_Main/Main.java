9ed7f2cd96aff2544d436967d839da76fb4a931f
Lucas5522 <luceitormaser@gmail.com>
2024-04-16 13:17:03 -0300

    finalizacion ejercicio 13 Creacion de package y class ejercicio 14

--- tp1_MontesLucas/src/ar/edu/unju/fi/ejercicio13_Main/Main.java
+++ tp1_MontesLucas/src/ar/edu/unju/fi/ejercicio13_Main/Main.java
@@ -1,10 +1,23 @@
 package ar.edu.unju.fi.ejercicio13_Main;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+		Scanner scanner = new Scanner(System.in);
+		int[] numLsit = new int[8];
 
+		for(int i = 0 ; i < numLsit.length ; i++ ) {
+			System.out.println("Ingrese valores enteros: ");
+			int n = scanner.nextInt();
+			numLsit[i] = n;
+		}
+		for(int j = 0 ; j < numLsit.length ; j++) {
+			System.out.println("Posicion: " + j + " valor: " + numLsit[j]);
+		}
+		
+		
 	}
 
 }
---
+++ tp1_MontesLucas/src/ar/edu/unju/fi/ejercicio14_Main/Main.java
@@ -0,0 +1,10 @@
+package ar.edu.unju.fi.ejercicio14_Main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
