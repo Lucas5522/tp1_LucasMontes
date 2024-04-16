c4c3ad004b230e56262577127817318192a672a6
Lucas5522 <luceitormaser@gmail.com>
2024-04-16 13:24:59 -0300

    finalizacion ejercicio 14 creacion package y class para el ejercicio 15

--- tp1_MontesLucas/src/ar/edu/unju/fi/ejercicio14_Main/Main.java
+++ tp1_MontesLucas/src/ar/edu/unju/fi/ejercicio14_Main/Main.java
@@ -1,9 +1,35 @@
 package ar.edu.unju.fi.ejercicio14_Main;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int x;
+		int total = 0;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ingrese un valor entre(3,10)");
+		x = scanner.nextInt();
+		int[] listi = new int[x];
+		
+		
+		for(int i = 0 ; i < listi.length ; i++ ) {
+			System.out.println("Ingrese valores enteros: ");
+			int n = scanner.nextInt();
+			listi[i] = n;
+			total= total + listi[i];
+		}
+		
+		for(int j = 0 ; j < listi.length ; j++) {
+			System.out.println( "valor: " + listi[j]);
+		}
+		System.out.println("suma total: " + total);
+		
+		
+		
+		
+		
+
 
 	}
 
