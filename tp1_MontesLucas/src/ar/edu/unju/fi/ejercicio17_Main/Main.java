9121202c9178ecb370a9a1a1ee2821f84ff9c295
Lucas5522 <luceitormaser@gmail.com>
2024-04-17 15:35:42 -0300

    Creacion del menu, del arraylist, error al mostrar el arraylist

--- tp1_MontesLucas/src/ar/edu/unju/fi/ejercicio17_Main/Main.java
+++ tp1_MontesLucas/src/ar/edu/unju/fi/ejercicio17_Main/Main.java
@@ -1,10 +1,85 @@
 package ar.edu.unju.fi.ejercicio17_Main;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import ar.edu.unju.fi.ejercicio17_Model.Jugador;
 
+public class Main {
+	private static Scanner scanner;
+	private static List<Jugador> jugador;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int op;
+		scanner = new Scanner(System.in);
+		
+		
+		do {
+			System.out.println("*********************************************************************************");
+			System.out.println("opcion 1: Alta del jugador");
+			System.out.println("opcion 2: Mostrar datos del juagdor");
+			System.out.println("opcion 3: Mostar los juagdores ordenados por apellido");
+			System.out.println("opcion 4: Modificar los datos de un jugador");
+			System.out.println("opcion 5: Eliminar un jugador");
+			System.out.println("opcion 6: Mostar la cantidad de jugadores");
+			System.out.println("opcion 7: Mostrar la cantidad de jugadores que pertenece a una nacionalidad");
+			System.out.println("opcion 8: Salir");
+			System.out.println("*********************************************************************************");
+			System.out.println("Ingrese opcion");
+			op = scanner.nextInt();
+			switch (op) {
+			case 1:
+			break;
+			case 2: mostrarJugador();
+			break;
+			case 3: 
+			case 4: 
+			case 5: 
+			case 6: 
+			case 7: 
+			case 8: System.out.println("Fin del programam");
+			break;
+			
+			
+			default:
+				System.out.println("Ingrece una opcion correcta");
+			}
+			
+			
+			
+			
+			
+			
+			
+		} while (op==8);
+		
+		
+		
+		
+		
+		
 
 	}
+	
+	public static void preCargaJugador() {
+		if(jugador == null) {
+				jugador = new ArrayList<>(); 
+		}
+		jugador.add(new Jugador("Lucas", "Montes", "19-07-1995", "Argentino", 1.80f, 79f, "defensa"));
+		jugador.add(new Jugador("Antonio", "Mamani", "13-09-1990", "Argentino", 1.70f, 70f, "arquero"));
+		jugador.add(new Jugador("Leonel", "Messi", "19-07-1995", "Argentino", 1.80f, 79f, "delantero"));
+		jugador.add(new Jugador("Ector", "Pala", "19-07-1995", "Argentino", 1.80f, 79f, "defensa"));
+	}
+	
+	public static void mostrarJugador() {
+		System.out.println("*************LISTA DE JUGADORES****************");
+		//jugadores.forEach(j->System.out.println(j));
+		for(Jugador jug:jugador) {
+			System.out.println("Nombre: " + jug.getNombre());
+		}
+	}
+	
+	
+	
 
 }
--- tp1_MontesLucas/src/ar/edu/unju/fi/ejercicio17_Model/Jugador.java
+++ tp1_MontesLucas/src/ar/edu/unju/fi/ejercicio17_Model/Jugador.java
@@ -1,5 +1,119 @@
 package ar.edu.unju.fi.ejercicio17_Model;
 
-public class Jugador {
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Scanner;
 
+public class Jugador {
+	private String nombre;
+	private String apellido;
+	private String fechaNacimiento;
+	private String nacionalidad;
+	private float estatura;
+	private float peso;
+	private String posicion;
+	Scanner scanner = new Scanner(System.in);
+	
+	public void calcularEdad() {
+		LocalDate fechaActual = LocalDate.now();
+		LocalDate fecha = LocalDate.parse(fechaNacimiento);
+		Period anio = Period.between(fecha, fechaActual);
+		int año = anio.getYears();
+		System.out.println("Edad: " + año);
+	}	
+	
+		
+	@Override
+	public String toString() {
+		return "Jugador [nombre=" + nombre + ", apellido=" + apellido + ", fechaNacimiento=" + fechaNacimiento
+				+ ", nacionalidad=" + nacionalidad + ", estatura=" + estatura + ", peso=" + peso + ", posicion="
+				+ posicion + "]";
+	}
+
+	public Jugador() {
+	}
+	
+	public Jugador(String nombre, String apellido, String fechaNacimiento, String nacionalidad, float estatura,
+			float peso, String posicion) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.fechaNacimiento = fechaNacimiento;
+		this.nacionalidad = nacionalidad;
+		this.estatura = estatura;
+		this.peso = peso;
+		this.posicion = posicion;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getApellido() {
+		return apellido;
+	}
+
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+
+	public String getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+
+	public void setFechaNacimiento(String fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+
+
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+
+
+	public float getEstatura() {
+		return estatura;
+	}
+
+
+	public void setEstatura(float estatura) {
+		this.estatura = estatura;
+	}
+
+
+	public float getPeso() {
+		return peso;
+	}
+
+
+	public void setPeso(float peso) {
+		this.peso = peso;
+	}
+
+
+	public String getPosicion() {
+		return posicion;
+	}
+
+
+	public void setPosicion(String posicion) {
+		this.posicion = posicion;
+	}	
+	
+	
 }
